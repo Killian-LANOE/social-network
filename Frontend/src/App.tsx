4d694738c6fa3cc70dcb71c0ca107f3eebd7e028
Killian Lanoe <killian.lanoe98@gmail.com>
2024-07-14 14:24:01 +0200

    Modified Pre written code to test fetch for images
    Removed base code.
    Added a test function for images fetch.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useState } from "react";
+
+type imgType = {
+  id: string;
+  name: string;
+  mimetype: string;
+  path: string;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imgData, setImgData] = useState<imgType>();
+
+  async function fetchData() {
+    await fetch("http://127.0.0.1:3000/api/images")
+      .then((data) => {
+        return data.json();
+      })
+      .then((dataJson) => {
+        setImgData(dataJson);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {imgData?.map((img, index) => {
+          return (
+            <img
+              key={index}
+              src={`${img.path}`}
+              alt={`image number ${index}`}
+            />
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={fetchData}>Get Post</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
