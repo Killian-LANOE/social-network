df75b0e6f5b7262cfa13cb9f8d31123955e47a0f
Killian Lanoe <killian.lanoe98@gmail.com>
2024-07-15 19:55:20 +0200

    Created an authentication middleware
    Created a function to identify the user from it's token

---
+++ Backend/middleware/auth.js
@@ -0,0 +1,16 @@
+const jsonwebtoken = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jsonwebtoken.verify(token, process.env.SECRET_TOKEN);
+    const userId = decodedToken.userId;
+    req.auth = {
+      userId: userId,
+    };
+    next();
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+};
