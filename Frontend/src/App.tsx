9c0fa1bd43c6a913c3e163abc13de0fc9fce60c2
Killian Lanoe <killian.lanoe98@gmail.com>
2024-07-20 17:10:30 +0200

    Added onload fetch posts, Modified Delete function

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import DeletePost from "./components/DeletePost";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type postsType = [
   {
@@ -15,47 +15,53 @@ type postsType = [
 function App() {
   const [postsData, setPostsData] = useState<postsType>();
 
-  const token = localStorage.getItem("token");
+  useEffect(() => {
+    async function fetchData() {
+      const token = localStorage.getItem("token");
 
-  async function fetchData() {
-    await fetch("http://127.0.0.1:3000/api/posts", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((data) => {
-        return data.json();
+      await fetch("http://127.0.0.1:3000/api/posts", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       })
-      .then((dataJson) => {
-        console.log(dataJson);
-        setPostsData(dataJson);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+        .then((data) => {
+          return data.json();
+        })
+        .then((dataJson) => {
+          setPostsData(dataJson);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        {postsData?.map((post, index) => {
-          return (
-            <div key={index}>
-              <figure>
-                <img
-                  src={`${post.images_path}`}
-                  alt={`image number ${index}`}
-                  height={480}
-                />
-                <figcaption>{post.description.split('"')}</figcaption>
-              </figure>
+      {postsData && postsData.length > 0 ? (
+        <div>
+          {postsData.map((post, index) => {
+            const description = post.description.split('"');
+            return (
+              <div key={index}>
+                <figure>
+                  <img
+                    src={`${post.images_path}`}
+                    alt={`image number ${index}`}
+                    height={480}
+                  />
+                  <figcaption>{description}</figcaption>
+                </figure>
 
-              <DeletePost id={post.id} />
-            </div>
-          );
-        })}
-      </div>
-      <button onClick={fetchData}>Get Post</button>
+                <DeletePost id={post.id} />
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>No Post Found</div>
+      )}
     </>
   );
 }
--- Frontend/src/components/DeletePost.tsx
+++ Frontend/src/components/DeletePost.tsx
@@ -1,19 +1,22 @@
-type DeletePostTypes = {
+type DeletePostType = {
   id: string;
 };
 
-function DeletePost(id: DeletePostTypes) {
+function DeletePost({ id }: DeletePostType) {
   const token = localStorage.getItem("token");
 
   function handleDelete() {
-    fetch(`http://127.0.0.1:3000/api/posts?${id}`, {
+    fetch(`http://127.0.0.1:3000/api/posts/${id}`, {
       method: "DELETE",
       headers: {
         Authorization: `Bearer ${token}`,
       },
     })
       .then((res) => {
-        if (res.ok) return alert("Posts successfuly deleted");
+        if (res.ok) {
+          alert("Posts successfuly deleted");
+          location.reload();
+        }
       })
       .catch((error) => {
         alert(error);
