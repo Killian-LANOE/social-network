63deccf0685fee56a5ebcf2f94571c62680e2c36
Killian Lanoe <killian.lanoe98@gmail.com>
2024-07-26 15:16:37 +0200

    Removed app.css / Created routes in App

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,72 +1,21 @@
-import "./App.css";
-import DeletePost from "./components/DeletePost";
-import ModifyPost from "./components/ModifyPost";
-
-import { useState, useEffect } from "react";
-
-type postsType = [
-  {
-    id: string;
-    description: string;
-    user_id: string;
-    images_path: string;
-  }
-];
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import Header from "./components/Header";
 
 function App() {
-  const [postsData, setPostsData] = useState<postsType>();
-
-  useEffect(() => {
-    async function fetchData() {
-      const token = localStorage.getItem("token");
-
-      await fetch("http://127.0.0.1:3000/api/posts", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((data) => {
-          return data.json();
-        })
-        .then((dataJson) => {
-          setPostsData(dataJson);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    fetchData();
-  }, []);
-
   return (
-    <>
-      {postsData && postsData.length > 0 ? (
-        <div>
-          {postsData.map((post, index) => {
-            const description = post.description.split('"');
-            return (
-              <div key={index}>
-                <a href={post.images_path}>
-                  <figure>
-                    <img
-                      src={`${post.images_path}`}
-                      alt={`image number ${index}`}
-                      height={480}
-                    />
-                    <figcaption>{description}</figcaption>
-                  </figure>
-                </a>
-
-                <DeletePost id={post.id} />
-                <ModifyPost id={post.id} />
-              </div>
-            );
-          })}
-        </div>
-      ) : (
-        <div>No Post Found</div>
-      )}
-    </>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/posts/:id" element={<Post />} />
+      </Routes>
+    </div>
   );
 }
 
