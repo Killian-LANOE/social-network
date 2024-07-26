d8b1b50412335c4cf5d38847c68da141054c4df8
Killian Lanoe <killian.lanoe98@gmail.com>
2024-07-26 18:04:58 +0200

    Created Login & Logout handling in Login & header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
   const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
   return (
     <header>
       <nav className="flex gap-6">
         {token ? (
           <>
             <Link to="/">Home</Link>
-            <Link to="/Login">Logout</Link>
+            <button onClick={handleLogout}>Logout</button>
           </>
         ) : (
           <Link to="/Login">Login</Link>
--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,5 +1,47 @@
+import { FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  return <main>Login</main>;
+  const navigate = useNavigate();
+  const emailRegex = `(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))`;
+
+  function handleSignup(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as HTMLFormElement;
+
+    fetch("http://127.0.0.1:3000/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: target["email"].value,
+        password: target["password"].value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        navigate("/");
+      })
+      .catch((error) => console.log(error));
+  }
+
+  return (
+    <main>
+      <form
+        className="flex flex-col gap-3 text-black"
+        onSubmit={(e) => handleSignup(e)}
+      >
+        <input
+          pattern={emailRegex}
+          type="email"
+          name="email"
+          placeholder="email"
+        ></input>
+        <input type="password" name="password" placeholder="password"></input>
+        <button className="border-2 text-white">Login</button>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
