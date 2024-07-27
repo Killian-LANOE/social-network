bbdd5ec6a4a2d7b02fa531b9a1b20859acc14852
Killian Lanoe <killian.lanoe98@gmail.com>
2024-07-27 14:45:07 +0200

    Modified modify post form and data handling

--- Frontend/src/components/ModifyPost.tsx
+++ Frontend/src/components/ModifyPost.tsx
@@ -1,3 +1,5 @@
+import { FormEvent } from "react";
+
 type ModifyPostType = {
   id: string;
 };
@@ -5,12 +7,20 @@ type ModifyPostType = {
 function ModifyPost({ id }: ModifyPostType) {
   const token = localStorage.getItem("token");
 
-  function handleModification() {
+  function handleModification(e: FormEvent) {
+    e.preventDefault();
+    const target = e.target as HTMLFormElement;
+
+    const data = new FormData();
+    data.append("description", target["description"].value);
+    data.append("image", target["image"].files[0]);
+
     fetch(`http://127.0.0.1:3000/api/posts/${id}`, {
       method: "PUT",
       headers: {
         Authorization: `Bearer ${token}`,
       },
+      body: data,
     })
       .then((res) => {
         if (res.ok) {
@@ -25,11 +35,15 @@ function ModifyPost({ id }: ModifyPostType) {
 
   return (
     <>
-      <form className="flex flex-col my-2 gap-4" encType="multipart/form-data">
+      <form
+        onSubmit={(e) => handleModification(e)}
+        className="flex flex-col my-2 gap-4 text-black"
+        encType="multipart/form-data"
+      >
         <input name="description" type="text" placeholder="Description"></input>
         <input name="image" type="file"></input>
+        <button className="text-white">Modify</button>
       </form>
-      <button onClick={handleModification}>Modify</button>
     </>
   );
 }
