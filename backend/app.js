bcbbbe30554ad07f4790d440d8b642a6ae26575d
Killian Lanoe <killian.lanoe98@gmail.com>
2025-03-16 12:37:06 +0100

    Added DB connection & fixed function  error on Posts controllers

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const postsRoutes = require("./router/posts");
 const usersRoutes = require("./router/users");
+
+const { Client } = require("pg");
+const client = new Client({
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+});
+
+client.connect();
+
+client.query(`SELECT * FROM users;`, (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err);
+  }
+  client.end();
+});
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,19 +1,19 @@
-exports.getPosts((req, res, next) => {
+exports.getPosts = (req, res, next) => {
   res.json({ message: "Posts" });
-});
+};
 
-exports.getSinglePost((req, res, next) => {
+exports.getSinglePost = (req, res, next) => {
   res.json({ message: "Post" });
-});
+};
 
-exports.createPost((req, res, next) => {
+exports.createPost = (req, res, next) => {
   res.json({ message: "Post Created" });
-});
+};
 
-exports.deletePost((req, res, next) => {
+exports.deletePost = (req, res, next) => {
   res.json({ message: "Post Deleted" });
-});
+};
 
-exports.modifyPost((req, res, next) => {
+exports.modifyPost = (req, res, next) => {
   res.json({ message: "Post Modified" });
-});
+};
