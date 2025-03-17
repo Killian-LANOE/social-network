94ba03fcccdbd0298023e6d8ae5acca9ceb32cf1
Killian Lanoe <killian.lanoe98@gmail.com>
2025-03-17 11:29:59 +0100

    Fixed create & modify  posts to not have empty post & Delete past image on modify in case of change

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,3 +1,4 @@
+const { error } = require("console");
 const database = require("../DB_Connection");
 const fs = require("fs");
 
@@ -23,9 +24,14 @@ exports.getSinglePost = async (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
   try {
-    const imageUrl = `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`;
+    const imageUrl = req.file
+      ? `${req.protocol}://${req.get("host")}/images/${req.file?.filename}`
+      : null;
+
+    if (req.file === undefined && req.body.description === undefined) {
+      return res.status(400).json({ error: "You can't create an empty post" });
+    }
+
     database.none(
       `INSERT INTO posts(userid, description, image_url) VALUES($1, $2, $3)`,
       [req.auth.userid, req.body.description, imageUrl]
@@ -69,19 +75,33 @@ exports.modifyPost = async (req, res, next) => {
     if (req.auth.userid != postData.userid || req.auth.is_admin != true) {
       return res.status(401).json("You are not authorized to do that");
     }
+
+    if (req.file === undefined && req.body.description === undefined) {
+      return res.status(401).json({
+        error: "You can't modify without changing the description or image",
+      });
+    }
+
+    let imageUrl = req.file
+      ? `${req.protocol}://${req.get("host")}/images/${req.file?.filename}`
+      : null;
+
     if (req.body.description === undefined) {
       req.body.description = postData.description;
     }
-    if (req.body.image_url === undefined) {
-      req.body.image_url = postData.image_url;
+
+    if (imageUrl === null) {
+      imageUrl = postData.image_url;
+    } else {
+      const filename = postData.image_url.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {
+        database.none(
+          `UPDATE posts SET description = $1, image_url = $2 WHERE postid = $3`,
+          [req.body.description, imageUrl, req.params.id]
+        );
+      });
+      return res.status(200).json({ message: "Post Modified Successfully" });
     }
-
-    database.none(
-      `UPDATE posts SET description = $1, image_url = $2 WHERE postid = $3`,
-      [req.body.description, req.body.image_url, req.params.id]
-    );
-
-    return res.status(200).json({ message: "Post Modified Successfully" });
   } catch (error) {
     return res.status(500).json({ error });
   }
